0ae328115fd793287334a9fcd229a9d84d7705d9
Mark <jungtaehoon@icloud.com>
2017-11-28 14:16:54 +0900

    first commit

---
+++ spec/getElementsByClassNameSpec.js
@@ -0,0 +1,31 @@
+var htmlStrings = [
+  '<div class="targetClassName"></div>',
+  '<div class="otherClassName targetClassName"></div>',
+  '<div><div class="targetClassName"></div></div>',
+  '<div><div class="targetClassName"><div class="targetClassName"></div></div></div>',
+  '<div><div></div><div><div class="targetClassName"></div></div></div>',
+  '<div><div class="targetClassName"></div><div class="targetClassName"></div></div>',
+  '<div><div class="somediv"><div class="innerdiv"><span class="targetClassName">yay</span></div></div></div>'
+];
+
+describe('getElementsByClassName', function() {
+  //뭐지
+
+  it('반환된 결과가 빌트-인 getElementsByClassName함수의 결과와 동일해야 합니다', function() {
+    $('body').addClass('targetClassName');
+    htmlStrings.forEach(function(htmlString) {
+      var $rootElement = $(htmlString);
+      $('body').append($rootElement);
+
+      var result = getElementsByClassName('targetClassName');
+      var expectedNodeList = document.getElementsByClassName('targetClassName');
+      var expectedArray = Array.prototype.slice.apply(expectedNodeList);
+      var equality = _.isEqual(result, expectedArray);
+      expect(equality).to.equal(true);
+
+      $rootElement.remove();
+    });
+    $('body').removeClass('targetClassName');
+  });
+
+});
---
+++ src/getElementsByClassName.js
@@ -0,0 +1,10 @@
+// 아래의 주석 처리된 코드를 실행하면 테스트가 완벽히 통과됩니다. 왜일까요? 한번 생각해보세요.
+// var getElementsByClassName = function (className) {
+//   return document.getElementsByClassName(className);
+// };
+
+// 
+var getElementsByClassName = function(className) {
+  // 여기에 코드를 작성해주세요.
+  
+};
